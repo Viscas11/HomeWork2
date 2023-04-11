d2a554fd69b08098657c79a85bff4d2c9e51b617
Syat <titmouse99@mail.ru>
2023-04-11 21:21:53 +0300

    1 задача

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int a = -1325693345;
+        byte b = 46;
+        short c = 11111;
+        long d = -1234567890987L;
+        float e = 2.5f;
+        double g = -0.4f;
+           System.out.println(a);
+           System.out.println(b);
+           System.out.println(c);
+           System.out.println(d);
+           System.out.println(e);
+           System.out.println(g);
+    }
+}
