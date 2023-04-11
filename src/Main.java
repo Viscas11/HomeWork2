b72310155dcc47096d7dd7ee0b894e3ea0c8770e
Syat <titmouse99@mail.ru>
2023-04-11 21:30:07 +0300

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1 () {
@@ -19,5 +20,22 @@ public class Main {
            System.out.println(d);
            System.out.println(e);
            System.out.println(g);
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
+        float a = 27.12f;
+        long b = 987678965549L;
+        float c = 2.786f;
+        short d = 569;
+        short e = -159;
+        short f = 27897;
+        byte g = 67;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
     }
 }
